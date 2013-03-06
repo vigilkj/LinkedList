7d8fe1c2148369d32c0ee2e449099a5f2717f0c4
vigilkj <vigilkj@s.dcsdk12.org>
2013-03-06 12:47:07 -0700

    wrote node

--- Node.java
+++ Node.java
@@ -9,6 +9,43 @@
  */
 
 
-class Node {
+public class Node {
 	
+	private int value;
+	private Node next;
+
+	public Node(int value, Node next){
+		this.value = value;
+		this.next = next;
+	}
+
+	public Node(){
+		this(0, null);
+	}
+
+	public Node(int value){
+		this(value, null);
+	}
+
+	/**
+	 * Getters and setters
+	 */
+	public int getValue(){
+		return this.value;
+	}
+
+	public void setValue(int value){
+		this.value = value;
+	}
+
+	public Node getNext(){
+		return this.next;
+	}
+
+	public void setNext(Node next){
+		this.next = next;
+	}
+
+
+
 }
